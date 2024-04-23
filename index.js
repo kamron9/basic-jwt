830f133ff6e833bd0e1bc33afb1ebd678e159f64
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-04-23 19:40:52 +0500

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
-	res.redirect('/auth')
+	res.send('server is running')
 })
 
 app.use('/auth', authRouter)
