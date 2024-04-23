71730277b824082fc8f621e6480d381c655afb05
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-04-23 19:55:09 +0500

    minor bug fixes

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const authRouter = require('./authRouter')
 const cors = require('cors')
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 const URL =
-	'mongodb+srv://kamron:kamron99@auth.cewe8xi.mongodb.net/?retryWrites=true&w=majority'
+	'mongodb+srv://kamron:kamron99@auth.cewe8xi.mongodb.net/?retryWrites=true&w=majority&appName=auth'
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -17,7 +17,7 @@ app.use('/auth', authRouter)
 const start = async () => {
 	try {
 		await mongoose.connect(URL)
-		app.listen(PORT, () => console.log(`server running ${PORT}`))
+		app.listen(5000, () => console.log(`server running ${5000}`))
 	} catch (e) {
 		console.log(e)
 	}
