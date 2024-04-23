2ff6f4cbe49bf5ea62d45af34112f32ede673425
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-04-23 19:36:15 +0500

    procfile added

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const authRouter = require("./authRouter");
-const cors = require("cors");
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./authRouter')
+const cors = require('cors')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 const URL =
-  "mongodb+srv://kamron:kamron99@auth.cewe8xi.mongodb.net/?retryWrites=true&w=majority";
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/auth", authRouter);
+	'mongodb+srv://kamron:kamron99@auth.cewe8xi.mongodb.net/?retryWrites=true&w=majority'
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.get('/', (req, res) => {
+	res.redirect('/auth')
+})
+
+app.use('/auth', authRouter)
 const start = async () => {
-  try {
-    await mongoose.connect(URL);
-    app.listen(PORT, () => console.log(`server running ${PORT}`));
-  } catch (e) {
-    console.log(e);
-  }
-};
-start();
+	try {
+		await mongoose.connect(URL)
+		app.listen(PORT, () => console.log(`server running ${PORT}`))
+	} catch (e) {
+		console.log(e)
+	}
+}
+start()
